6567c955a4b22a42ecad8bc2a6eb6ded059b1768
ishan <ishan@DESKTOP-TOATOCU>
2023-03-16 12:57:32 +0530

    Add project files.

---
+++ Assignment_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Assignment_1
+{
+
+
+
+
+
+    internal class Program6
+    {
+        static void Main(string[] args)
+        {
+
+            /*            Assignment_1*/
+
+
+            Console.WriteLine("assignment = 1");
+
+            string path = @"D:\file1.txt";
+
+
+            byte[] readtext = File.ReadAllBytes(path);
+
+
+            var str = System.Text.Encoding.Default.GetString(readtext);
+            Console.WriteLine(str);
+
+
+            File.WriteAllBytes(@"D:\file3.txt", readtext);
+
+
+
+
+            Console.WriteLine("done");
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
